6fcb79d3ec34258a1eabb346587af607c0e16e8a
AGG341 <angela.garcia35@my.unitec.edu.mx>
2024-10-21 12:02:24 -0600

    Create Practica A.cpp

---
+++ Practica A.cpp
@@ -0,0 +1,155 @@
+//PRACTICA # A 
+//Ejercicios de Introducción C++: 
+//1.1 [TIPOS DE VARIABLE Y CODIFICACIÓN, Conteste la pregunta y genere la respuesta en código en C++] Declare una variable tipo int que tenga el número de materias que estás cursando actualmente.
+
+int numeroMaterias = 7;
+
+//1.2 [TIPOS DE VARIABLE Y CODIFICACIÓN, Conteste la pregunta y genere la respuesta en código en C++] Declare una variable tipo int que guarde el valor del cuatrimestre que estas cursando
+
+int numeroCuatrimestre = 5;
+
+//1.3 [CODIFICACIÓN, genere la respuesta en código en C++] Declara una variable tipo string con identificador “materia”, e inicializar con el nombre de esta materia.
+
+string materia = “Fundamentos de programación”;
+
+//1.4 [CODIFICACIÓN, genere la respuesta en código en C++] Declara una variable tipo string con identificador “cuenta”, e inicializar con el tu número de cuenta/matricula del Unitec
+
+string cuenta= “335015416”;
+
+//1.5 [CODIFICACIÓN, genere la respuesta en código en C++] Declara una variable de tipo string con identificador “nombre”, e inicializar con tu nombre(s) propio(s).
+
+string nombre = “Angela”;
+
+//1.7 [CODIFICACIÓN, genere la respuesta en código en C++] Declara una variable de tipo string con identificador “apellidoPaterno”, e inicializar con tu apellido paterno.
+
+string apellidoPaterno= “Garcia”;
+
+//1.12 [CODIFICACIÓN, genere la respuesta en código en C++] Declara una variable de tipo string con identificador “apellidoMaterno”, e inicializar con tu apellido materno.
+
+string apellidoMaterno= “Gastelum”;
+
+//1.8 [CODIFICACIÓN, genere la respuesta en código en C++] Declara una variable tipo long con identificador “celular”, e inicializar con tu número celular.
+
+long int celular = 5560551562;
+
+//1.9 [CODIFICACIÓN, genere la respuesta en código en C++] Declara una variable tipo long con identificador “casa”, e inicializar con tu número de casa.
+
+Long int casa = 5553491532;
+
+
+//1.10 [CODIFICACIÓN, genere la respuesta en código en C++] Declara una variable tipo string con identificador “correMyUnitec”, e inicializar con el correo electrónico que tienes asignado en el Unitec.
+
+string correoMyUnitec = “angela.garcia35@my.unitec.edu.mx”;
+
+//1.11 [CODIFICACIÓN, genere la respuesta en código en C++] Declara una variable tipo string con identificador “carrera”, e inicializar con la carrera que cursas actualmente en el Unitec.
+
+string carrera = “Animación, Diseño y Arte Digital”;
+
+//1.12 [CODIFICACIÓN, genere la respuesta en código en C++] Declara una variable tipo bool con identificador “blackboard”, e inicializar con valor true, si lo has usado anteriormente, de lo contrario inicializar con valor false. 
+
+bool blackboard = true;
+
+//Ejercicios del Módulo: Tipo de Variables. 
+//Sección: Análisis de errores de sintaxis.
+
+// Ejercicio 1: 
+//int 1numero = 5; 
+
+int n1 = 5;
+
+// Ejercicio 2: 
+//float valor@ = 3.14;
+
+float valor = 3.14;
+
+// Ejercicio 3: 
+//double double = 9.81; 
+
+double variable = 9.81
+
+// Ejercicio 4: 
+//char letra = 'AB';
+
+string letra = “AB”;
+
+// Ejercicio 5: 
+//bool esVerdadero = "true";
+
+bool esVerdarero = true;
+
+// Ejercicio 6: 
+//int numero = 10.5;
+
+float numero = 10.5;
+
+// Ejercicio 7: 
+//string nombre = 'Juan';
+
+string nombre = “Juan”;
+
+// Ejercicio 8: 
+//const int PI = 3.14;
+
+const float PI = 3.14;
+
+// Ejercicio 9: 
+//int suma = 5 + ;
+
+int suma = 5 + n1;
+
+// Ejercicio 10: 
+//int numero; 
+//numero = 10
+
+int numero; 
+numero = 10;
+
+// Ejercicio 11: 
+//int numero = "diez"
+
+string numero = “diez”;
+
+// Ejercicio 12: 
+//float altura = 1,75;
+
+float altura = 1.75;
+
+// Ejercicio 13: 
+//char inicial = "J";
+
+string inicial = “J”;
+
+//Ejercicio 14: 
+//bool esMayor = 1;
+
+bool esMayor = true;
+
+// Ejercicio 15: 
+//string apellido = Juan;
+
+string apellido = “Juan”;
+
+// Ejercicio 16: 
+//int resultado = 5 * ;
+
+int resultado= 5;
+
+// Ejercicio 17: 
+//double area = 3.14 * radio^2;
+
+Double area = 3.14 * radio * radio;
+
+// Ejercicio 18: 
+//int edad = 25
+
+int edad = 25;
+
+// Ejercicio 19: 
+//float peso = 70.5f;
+
+float peso = 70.5;
+
+// Ejercicio 20: 
+//int suma = 10 + 20
+
+int suma = 10 + 20;
